e0bdb21d3f0df9806c09f4955436a2b5bbea9276
Melanie Smith <melanie.smith@sage.com>
2016-07-12 08:49:02 +0100

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+  def palindrome_tester(s)
+   if FILL_IN
+     puts "It's a palindrome!"
+   else
+     puts "It's not a palindrome."
+   end
+ end
 end
