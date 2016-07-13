5ece004cf22c6d63c56bed292139751a8f036d3b
Melanie Smith <melanie.smith@sage.com>
2016-07-13 14:56:24 +0100

    Finish user signup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user2@example.com",
+    @user = User.new(name: "Example User", email: "user@example.com",
                       password: "foobar", password_confirmation: "foobar")
   end
 
@@ -52,7 +52,7 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
-    assert duplicate_user.valid?
+    assert_not duplicate_user.valid?
   end
 
   test "emails should be lower case" do
