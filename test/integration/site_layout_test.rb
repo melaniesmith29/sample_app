297e11599c2daa2a97d11d67a6744d22cb016b0f
Melanie Smith <melanie.smith@sage.com>
2016-07-15 15:09:16 +0100

    Finish user edit, update, inex and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,6 +8,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
-
+    assert_select "a[href=?]", login_path
   end
 end
